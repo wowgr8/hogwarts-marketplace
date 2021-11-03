eefc79a7f7b6fa7d9efe403cc53da1f7aefaa906
Ebru Rice <ebruerenb@gmail.com>
2021-11-02 21:16:21 -0700

    Write JavaScript functions
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("#user").submit(function(event) {
     event.preventDefault();
-    const first = ("#first").val();
-    const last = ("last").val();
-    
+    const first = $("input#first").val();
+    const last = $("input#last").val();
+    const addr = $("input#addr").val();
+
+    $(".first-span").text(first);
+    $(".last-span").text(last);
+    $(".addr-span").text(addr);
+
+    $("#receipt").show();
   });
 });
