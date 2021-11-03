c93b0f7e06ee9bf006023059c397828235fa4d1c
Ebru Rice <ebruerenb@gmail.com>
2021-11-02 19:50:55 -0700

    Add cards and images, preliminary JavaScript
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
-}
+  $("#user").submit(function(event) {
+    event.preventDefault();
+    const first = ("#first").val();
+    const last = ("last").val();
+    
+  });
+});
