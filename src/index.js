3df53d8b78170159798bc572d1533b5ad38881fe
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-13 21:04:28 -0800

    add environment to old project - practice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
 $(document).ready(function() {
   $("#user").submit(function(event) {
     event.preventDefault();
